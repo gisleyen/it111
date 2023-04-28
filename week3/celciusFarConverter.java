bdb130ab7bf15738aba200d02b66d6d2c452d480
gisleyen <130393790+gisleyen@users.noreply.github.com>
2023-04-28 15:41:07 -0700

    Add files via upload

---
+++ week3/celciusFarConverter.java
@@ -0,0 +1,19 @@
+public class celciusFarConverter {
+
+    public static void main(String[]args) {
+
+        // far = (cel * 9/5) * 32
+
+        int cel, far;
+
+        for(cel = 0; cel <= 100; cel = cel+2) {
+            far = (cel * 9/5) + 32;
+            System.out.println(cel + " degrees Celcius is equal to " +far+ " degrees Fahrenheit");
+
+
+        }// end for loop
+        System.out.println("All done!");
+    }
+
+
+}
